0166e3a1e137508530ca1f8237b813d752b9d1aa
Nidhisharora <nidhisharora79@gmail.com>
2026-07-27 16:19:06 +0530

    Footer added in mode page

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+
 export default function Navbar() {
   const pathname = usePathname()
 
