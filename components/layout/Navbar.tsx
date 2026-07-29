2cc3fa325145cdc76af07f0fb4e3f5dda7229efc
Vishwas Naveen <vishwas.naveen2580@gmail.com>
2026-07-29 15:56:42 +0530

    feat: Added Rainbow Table demonstration

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -59,6 +59,7 @@ export default function Navbar() {
     { name: 'Merkle Tree', href: '/merkle' },
     { name: 'Padding', href: '/padding' },
     { name: 'Challenge', href: '/challenge' },
+    { name : 'Rainbow Table', href: '/rainbow-table' },
     { name: 'Docs', href: '/docs' },
     { name: 'Glossary', href: '/glossary' },
     { name: 'Resources', href: '/resources' },
